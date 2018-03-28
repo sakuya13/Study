52a1484565aa3655c84ceb75a78daa0080d23726
sakuya13 <sakuyasid1213@gmail.com>
2018-03-28 17:45:50 +1100

    Add java notes

---
+++ Java/reading/week5/JavadocDemo.java
@@ -0,0 +1,33 @@
+/**
+ 
+*/
+
+public class JavadocDemo {
+    ...
+
+
+    /**
+     Tests for equality of two objects of type Person. To be equal,
+     the two objects must have the same name, same birth date, and same death date.
+ 
+     @param otherPerson The person being compared to the calling object.
+     @return Returns true if the calling object equals otherPerson.
+    */
+    public boolean equals(person otherPerson) {
+    ...
+    }
+    
+    /**
+    @param Parameter_Name Parameter_Description
+    @return Description_Of_Value_Returned
+    @throws Exception_Type Explanation
+    @deprecated
+    @see Package_Name.Class_Name
+    @author Author
+    @version Version_Information
+    */
+    public static void fat() {
+    ...
+    }
+
+}
