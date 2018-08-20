fd516e7fcac2c63f385c0291d13089bb5226e68d
sakuya13 <sakuyasid1213@gmail.com>
2018-08-20 15:29:30 +1000

    Update java notes

--- Java/assessed/1/Greetings.java
+++ Java/assessed/1/Greetings.java
@@ -1,9 +1,5 @@
-import java.util.Scanner;
-
-
 public class Greetings {
     public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
         System.out.println("Hello " + args[0] + " from " + args[1] + ".");
         System.out.println("I'VE ALWAYS WANTED TO GO TO " 
                             + args[1].toUpperCase() + ".");
---
+++ Java/workshops/week5/Movies.java
@@ -0,0 +1,48 @@
+import java.util.StringTokenizer;
+
+public class Movies {
+    private static String FAVOURABLE = "1. fat is cute, 2. fat is cute";
+
+    public static void printMovies() {
+        System.out.println("1. fat is cute");
+    }
+
+    public static void printMovies(int rank, String movie) {
+        System.out.println(rank + ". " + movie);
+    }
+    
+//    public static void printMovies(String Movies) {
+//        StringTokenizer tk = new StringTokenizer(movies, ", "); //the extra args will only add more delimiters but still keep the space
+//        int i = 1;
+//        while (tk.hasMoreTokens()) {
+//            System.out.println(i + ". " + tk.nextToken());
+//            i ++;
+//        }
+//    }
+
+    public static void printMovies(String movies) {
+        String[] movieStrings = movies.split(", "); // use split() to define a specific delimiter
+        for (int i=0; i < movieStrings.length; i++) {
+            System.out.println((i+1) + ". " + movieStrings[i]);
+        }
+    }
+
+    public static boolean isAFavourable(String movie) {
+        return FAVOURABLE.contains(movie);
+    }
+
+    public static void main(String[] args) {
+        printMovies();
+        printMovies(1, "fat is cute");
+        printMovies(2, "fat is cute");
+        System.out.println(isAFavourable("fat"));
+        printMovies("The Avenger, Iron Man, Thor");
+
+//        System.out.printf("%s, %d, %n", "fat", 12);
+//        System.out.printf("%s, %d, ", "fat", 12);
+//        System.out.println();
+
+    }
+}
+
+
