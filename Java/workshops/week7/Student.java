21ec2256e7151de7d0a4c897e05d36145a911c45
sakuya13 <sakuyasid1213@gmail.com>
2018-09-04 18:19:16 +1000

    update java files

---
+++ Java/workshops/week7/Student.java
@@ -0,0 +1,103 @@
+public class Student {
+    private String name = null;
+    private int number = 0;
+    private String username = null;
+    private Subject[] enrollList = new Subject[4];
+        
+    public Student() {
+        name = "undefined";
+        number = 0;
+        username = "undefined";
+
+        for (int i = 0; i < enrollList.length; i++) {
+            enrollList[i] = null;
+        }
+    }
+
+    public Student(String name, int number, String username, Subject[] enrollList) {
+        this.name = name;
+        this.number = number;
+        this.username = username;
+
+        for (int i = 0; i < enrollList.length; i++) {
+            this.enrollList[i] = enrollList[i];
+        }
+    }
+
+    public Student(Student student) {
+        this.name = student.name;
+        this.number = student.number;
+        this.username = student.username;
+
+        for (int i = 0; i < enrollList.length; i++) {
+            if (student.enrollList[i] != null){
+                this.enrollList[i] = student.enrollList[i];
+            }
+        }
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    
+    public Subject[] getEnrollList() {
+        Subject[] newList = new Subject[4];
+        for (int i = 0; i < newList.length; i++) {
+            if (enrollList[i] != null) {
+                newList[i] = new Subject(enrollList[i]);
+            }
+        }
+        return newList;
+    }
+
+    public String toString() {
+        return name + " " + Integer.toString(number) + " " + username + "\n" + enrollList[0] +
+                "\n" + enrollList[1] + "\n" + enrollList[2] + "\n" + enrollList[3];
+    }
+    
+    public boolean equals(Student student) {
+        return (name.equals(student.name) && (number == student.number) &&
+                username.equals(student.username));
+    }
+    
+    public void enroll(Subject subject) {
+       for (int i = 0; i < enrollList.length; i++) {
+           if (enrollList[i] == null) {
+               enrollList[i] = new Subject(subject);
+               break;
+           }
+       }
+    }
+    
+    public void unenroll(Subject subject) {
+        for (int i = 0; i < enrollList.length; i++ ) {
+            if (enrollList[i] != null) {
+                if (subject.equals(enrollList[i])) {
+                    enrollList[i] = null;
+                }
+            }
+        }
+    }
+
+}
---
+++ Java/workshops/week7/Subject.java
@@ -0,0 +1,84 @@
+public class Subject {
+    private String code = null;
+    private String title = null;
+    private String coordinator = null;
+    private Student[] studentList = new Student[1000];
+
+    public Subject() {
+        code = "undefined";
+        title = "undefined";
+        coordinator = "unassigned";
+        for (int i = 0; i < studentList.length; i++) {
+            studentList[i] = null;
+        }
+    }
+
+    public Subject(String code, String title, String coordinator, Student[] studentList) {
+        this.code = code;
+        this.title = title;
+        this.coordinator = coordinator;
+        for (int i = 0; i < studentList.length; i++) {
+            this.studentList[i] = studentList[i];
+        }
+    }
+   
+
+    // copy constructor
+    public Subject(Subject subject) {
+        this.code = subject.code;
+        this.title = subject.title;
+        this.coordinator = subject.coordinator;
+        for (int i = 0; i < studentList.length; i++) {
+            if (subject.studentList[i] != null) {
+                this.studentList[i] = subject.studentList[i];
+            }
+        }
+    }
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCoordinator() {
+        return coordinator;
+    }
+
+    public Student[] getStudentList() {
+        Student[] copyList = new Student[1000];
+        for (int i = 0; i < studentList.length; i++) {
+            if (studentLis[i] != null) {
+                copyList[i] = new Student(studentList[i]);
+            }
+        }
+        return copyList;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCoordinator(String coordinator) {
+        this.coordinator = coordinator;
+    }
+
+    public String toString() {
+        return code + " " + title + " " + coordinator;
+    }
+
+    public boolean equals(Subject subject) {
+        return ((code.equals(subject.code)) && (title.equals(subject.title)) &&
+                 (coordinator.equals(subject.coordinator)));
+    }
+    
+
+
+}
